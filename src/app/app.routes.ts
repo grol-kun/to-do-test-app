17aed92ccacfa7381544192395bd8ab598fa9fa8
Artem Pushnev <artsiom.pushniou@innowise-group.com>
2023-12-05 16:30:55 +0300

    Forbid to reach login page for logged in users

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,6 +10,15 @@ export const routes: Routes = [
     path: 'login',
     title: 'Login',
     loadComponent: () => import('./auth/auth.component').then((c) => c.AuthComponent),
+    canMatch: [
+      (route: Route, segments: UrlSegment[]) => {
+        const router = inject(Router);
+        return inject(AuthService).isAuthenticated().pipe(
+          take(1),
+          map((isAuth) => !isAuth)
+        );
+      },
+    ],
   },
   {
     path: '',
